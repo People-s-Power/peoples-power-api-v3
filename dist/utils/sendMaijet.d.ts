72e3b9881f97075736c28afdf0f461055becdd60
greatAdams01 <kingifean@gmail.com>
2022-05-13 12:28:59 +0100

    Moved the whole app

---
+++ dist/utils/sendMaijet.d.ts
@@ -0,0 +1,4 @@
+export declare const testMessage: (email: any, name: any, productName: any, link: any) => Promise<void>;
+export declare const endorsedCampMail: (title: any, endorsementsCount: any, email: any, name: any) => Promise<void>;
+export declare const viewCampMail: (title: any, userName: any, email: any, name: any) => Promise<void>;
+export declare const updateCampMail: (title: any, email: any, name: any) => Promise<void>;
---
+++ src/utils/sendMaijet.ts
@@ -0,0 +1,132 @@
+/* eslint-disable prettier/prettier */
+import * as Mailjet from 'node-mailjet'
+import config from './config'
+
+const mailjet = Mailjet.connect(config.mailjet_api_key, config.mailjet_secret)
+
+export const testMessage = async (email, name, productName, link) => {
+  try {
+    const { body } = await mailjet
+    .post("send", {'version': 'v3.1'})
+    .request({
+      "Messages":[
+        {
+          "From": {
+            "Email": "kingifean@gmail.com",
+            "Name": "Transaction link"
+          },
+          "To": [
+            {
+              "Email": `${email}`,
+              "Name": `${name}`
+            }
+          ],
+          "Subject": `${productName}`,
+          "TextPart": `Payment for ${productName}`,
+          "HTMLPart": `Here's the link to pay for the product ${link}`,
+          "CustomID": "Transaction payment"
+        }
+      ]
+    })
+    console.log(body)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const endorsedCampMail = async (title, endorsementsCount, email, name) => {
+  // campaigns/plitical-maters
+  const link = 'here'
+  try {
+    const { body } = await mailjet
+    .post("send", {'version': 'v3.1'})
+    .request({
+      "Messages":[
+        {
+          "From": {
+            "Email": "kingifean@out.com",
+            "Name": "Endorsements"
+          },
+          "To": [
+            {
+              "Email": `${email}`,
+              "Name": `${name}`
+            }
+          ],
+          "Subject": `${title} was Endorsed`,
+          "TextPart": `${title} got another Endorsements`,
+          "HTMLPart": `Here's the link to Campiange ${link}`,
+          "CustomID": "Endorsements"
+        }
+      ]
+    })
+    console.log(body)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const viewCampMail = async (title, userName, email, name) => {
+  // campaigns/plitical-maters
+  const link = 'here'
+  try {
+    const { body } = await mailjet
+    .post("send", {'version': 'v3.1'})
+    .request({
+      "Messages":[
+        {
+          "From": {
+            "Email": "kingifean@out.com",
+            "Name": "View"
+          },
+          "To": [
+            {
+              "Email": `${email}`,
+              "Name": `${name}`
+            }
+          ],
+          "Subject": `${userName} Viewed your Campiange`,
+          "TextPart": `${title} got another View!!!`,
+          "HTMLPart": `Here's the link to Campiange ${link}`,
+          "CustomID": "Views"
+        }
+      ]
+    })
+    console.log(body)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateCampMail = async (title, email, name) => {
+  // campaigns/plitical-maters
+  const link = 'here'
+  try {
+    const { body } = await mailjet
+    .post("send", {'version': 'v3.1'})
+    .request({
+      "Messages":[
+        {
+          "From": {
+            "Email": "kingifean@out.com",
+            "Name": "Campiange Edit"
+          },
+          "To": [
+            {
+              "Email": `${email}`,
+              "Name": `${name}`
+            }
+          ],
+          "Subject": `${title} was Edited`,
+          "TextPart": `${title} was edited!!!`,
+          "HTMLPart": `Here's the link to Campiange ${link}`,
+          "CustomID": "CampiangeUpdate"
+        }
+      ]
+    })
+    console.log(body)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
