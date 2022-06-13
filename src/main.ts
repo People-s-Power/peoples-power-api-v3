4b5e710cfb3dc290d00e229daf1b9804b7fe9f64
greatAdams01 <kingifean@gmail.com>
2022-06-13 13:33:22 +0100

    opened cors

--- src/main.ts
+++ src/main.ts
@@ -46,11 +46,7 @@ async function bootstrap() {
     },
   });
 
-  app.enableCors({
-    origin,
-
-    credentials: true,
-  });
+  app.enableCors();
 
   // app.useWebSocketAdapter(new RedisIoAdapter(app));
   app.use(cookieParser());
